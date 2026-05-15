871adda55c41c72be7b8fd1ca9afcdec998a2985
aleprime2007 <aleprime2007@gmail.com>
2026-05-15 15:32:18 -0400

    Añadidas las dependencias faltantes en hero_install_dependencies.sh

--- hero_install_dependencies.sh
+++ hero_install_dependencies.sh
@@ -12,6 +12,8 @@ export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init -)"
 pyenv install 3.11.9
 cd "$PYENV_ROOT/versions/3.11.9/bin"
-./python pip install mediapipe==0.10.11 vosk pyaudio pyttsx3 opencv-python langchain python-dotenv
-curl -fsSL https://ollama.com/install.sh | bash
+./python pip install mediapipe==0.10.11 vosk pyaudio pyttsx3 opencv-python \
+langchain langchain_openai langchain_ollama langchain_huggingface python-dotenv
+curl -fsSL https://ollama.com/install.sh | sh
+sudo systemctl start ollama.service
 ollama run llama3.2:1b
